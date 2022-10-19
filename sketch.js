d0da4ed88c0d2db8fe33d4ba865a0e3d45550390
hartery5 <78227345+hartery5@users.noreply.github.com>
2022-10-19 10:06:57 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,108 @@
+let particles = [];
+let testParticles = [];
+let fixedCharges = [];
+
+let maxV = 0;
+
+let q = 1;
+let k = 2;
+
+let deltat = 5.0;
+let spacing = 20;
+
+let radio1;
+let checkbox;
+let checkbox2;
+
+let mover = true;
+let trashmode = false;
+
+function setup() {
+  textFont('Courier New')
+  createCanvas(400, 400);
+
+  radio1 = createRadio();
+  radio1.option(1, "q = +1");
+  radio1.option(-1.0, "q = -1");
+  radio1.selected("1");
+  radio1.style("width", "400px");
+  radio1.style('color', '#ffffff');
+  radio1.style('font-family', 'monospace');
+  radio1.position(0,height-20);
+  
+  checkbox = createCheckbox('Static', false);
+  checkbox.style("width", "400px");
+  checkbox.style('color', '#ffffff');
+  checkbox.style('font-family', 'monospace');
+  checkbox.position(0,height-40);
+  
+  checkbox2 = createCheckbox('Trash Mode', false);
+  checkbox2.style("width", "400px");
+  checkbox2.style('color', '#ffffff');
+  checkbox2.style('font-family', 'monospace');
+  checkbox2.position(0,height-60);
+  
+
+
+  for (let i = 10; i < width; i += spacing) {
+    for (let j = 10; j < width; j += spacing) {
+      p = new particle(i, j, q, false, true);
+      append(testParticles, p);
+    }
+  }
+  
+  particles = particles.concat(fixedCharges,particles);
+}
+
+function draw() {
+  background(0);
+  q = radio1.value();
+  if (checkbox.checked()){
+    mover = false;
+  }
+  else{
+    mover = true;
+  }
+  
+  if (checkbox2.checked()){
+    trashmode = true;
+    for (let i = 0; i < particles.length; i += 1) {
+      particles[i].move = false;
+    }
+  }
+  else{
+    trashmode = false;
+    for (let i = 0; i < particles.length; i += 1) {
+      if (particles[i].type){
+        particles[i].move = true;
+      }
+    }
+  }
+  
+  for (let i=0; i < testParticles.length; i+=1){
+    testParticles[i].update(particles, deltat);
+  }
+
+  for (let i = 0; i < particles.length; i += 1) {
+    particles[i].update(particles, deltat);
+    if (particles[i].flag){
+      particles.splice(i,1);
+    }
+  }
+}
+
+function mouseClicked() {
+  if (trashmode){
+    for (let i = 0; i < particles.length; i += 1) {
+      if ((abs(mouseX-particles[i].x)<particles[i].radius) && (abs(mouseY-particles[i].y)<particles[i].radius)){
+        particles[i].flag = true;
+      }
+    }
+  }
+  else if (mouseY<(height-60)){
+    let posX = round(mouseX/10)*10;
+    let posY = round(mouseY/10)*10;
+    p = new particle(posX,posY, q, mover, false);
+    append(particles, p);
+  }
+}
