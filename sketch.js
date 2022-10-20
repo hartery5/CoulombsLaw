2ea9cbb55e2b6a3b7ce8f76bacca2ad6010eb449
hartery5 <78227345+hartery5@users.noreply.github.com>
2022-10-20 11:06:29 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup() {
   textFont('Courier New')
   createCanvas(windowWidth, windowHeight);
   let density = displayDensity();
-  spacing = round(windowWidth*windowHeight/(density*10000);
+  spacing = round(width*height/(density*5000);
   
   radio1 = createRadio();
   radio1.option(1, "q = +1");
